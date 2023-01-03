65f440f60b0e495160fb681705116bda43811e14
Sergii Kochetov <sergii.koch@gmail.com>
2023-01-03 17:13:40 +0200

    Updated input

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,289 @@
 <template>
   <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
-    <router-view />
+    <div class="container mx-auto flex flex-col items-center p-4">
+      <div class="container">
+        <div class="w-full my-4"></div>
+        <section>
+          <div class="flex">
+            <div class="max-w-xs">
+              <label
+                for="wallet"
+                class="block text-sm font-medium text-gray-700"
+                >Ticker</label
+              >
+              <div class="mt-1 relative rounded-md shadow-md">
+                <input
+                  v-model="ticker"
+                  @keydown.enter="handleAdd"
+                  @input="handleMatch"
+                  type="text"
+                  name="wallet"
+                  id="wallet"
+                  class="block w-full pr-10 border-gray-300 text-gray-900 focus:outline-none focus:ring-gray-500 focus:border-gray-500 sm:text-sm rounded-md"
+                  placeholder="e.g. BTC"
+                />
+              </div>
+              <template v-if="matches.length > 0">
+                <div class="flex bg-white shadow-md p-1 rounded-md flex-wrap">
+                  <span
+                    v-for="(t, idx) in matches"
+                    @click="handleAdd"
+                    :key="idx"
+                    class="inline-flex items-center px-2 m-1 rounded-md text-xs font-medium bg-gray-300 text-gray-800 cursor-pointer"
+                  >
+                    {{ t }}
+                  </span>
+                </div>
+              </template>
+              <div class="text-sm text-red-600" v-if="this.isExisting">
+                The ticker you selected is already in the list
+              </div>
+            </div>
+          </div>
+          <button
+            @click="handleAdd"
+            type="button"
+            class="my-4 inline-flex items-center py-2 px-4 border border-transparent shadow-sm text-sm leading-4 font-medium rounded-full text-white bg-gray-600 hover:bg-gray-700 transition-colors duration-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
+          >
+            <svg
+              class="-ml-0.5 mr-2 h-6 w-6"
+              xmlns="http://www.w3.org/2000/svg"
+              width="30"
+              height="30"
+              viewBox="0 0 24 24"
+              fill="#ffffff"
+            >
+              <path
+                d="M13 7h-2v4H7v2h4v4h2v-4h4v-2h-4V7zm-1-5C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8z"
+              ></path>
+            </svg>
+            Add
+          </button>
+        </section>
+        <template v-if="tickers.length">
+          <hr class="w-full border-t border-gray-600 my-4" />
+          <dl class="mt-5 grid grid-cols-1 gap-5 sm:grid-cols-3">
+            <div
+              v-for="(t, idx) in tickers"
+              :key="idx"
+              @click="handleSelect(t)"
+              :class="{
+                'border-4': sel === t
+              }"
+              class="bg-white overflow-hidden shadow rounded-lg border-purple-800 border-solid cursor-pointer"
+            >
+              <div class="px-4 py-5 sm:p-6 text-center">
+                <dt class="text-sm font-medium text-gray-500 truncate">
+                  {{ t.name }} - USD
+                </dt>
+                <dd class="mt-1 text-3xl font-semibold text-gray-900">
+                  {{ t.price }}
+                </dd>
+              </div>
+              <div class="w-full border-t border-gray-200"></div>
+              <button
+                @click.stop="handleRemove(t)"
+                class="flex items-center justify-center font-medium w-full bg-gray-100 px-4 py-4 sm:px-6 text-md text-gray-500 hover:text-black hover:bg-gray-200 transition-all focus:outline-none"
+              >
+                <svg
+                  class="h-5 w-5"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 20 20"
+                  fill="#718096"
+                  aria-hidden="true"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z"
+                    clip-rule="evenodd"
+                  ></path>
+                </svg>
+                Remove
+              </button>
+            </div>
+          </dl>
+          <hr class="w-full border-t border-gray-600 my-4" />
+        </template>
+        <section v-if="sel" class="relative">
+          <h3 class="text-lg leading-6 font-medium text-gray-900 my-8">
+            {{ sel.name }} - USD
+          </h3>
+          <div class="sm:flex sm:space-x-4">
+            <div
+              v-for="(stat, idx) in selStats"
+              :key="idx"
+              class="inline-block bg-white rounded-lg text-left overflow-hidden shadow transform transition-all mb-1 w-full sm:w-1/3 sm:my-8"
+            >
+              <div class="bg-white p-5">
+                <div class="sm:flex sm:items-start">
+                  <div
+                    class="text-center sm:mt-0 sm:ml-2 sm:text-left uppercase"
+                  >
+                    <span class="text-sm font-medium text-gray-500 truncate">
+                      {{ stat.title }}
+                    </span>
+                    <p class="text-2xl font-semibold text-gray-900">
+                      {{ stat.price }}
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <button
+            @click="sel = null"
+            type="button"
+            class="absolute top-0 right-0"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              xmlns:xlink="http://www.w3.org/1999/xlink"
+              xmlns:svgjs="http://svgjs.com/svgjs"
+              version="1.1"
+              width="30"
+              height="30"
+              x="0"
+              y="0"
+              viewBox="0 0 511.76 511.76"
+              style="enable-background: new 0 0 512 512"
+              xml:space="preserve"
+            >
+              <g>
+                <path
+                  d="M436.896,74.869c-99.84-99.819-262.208-99.819-362.048,0c-99.797,99.819-99.797,262.229,0,362.048    c49.92,49.899,115.477,74.837,181.035,74.837s131.093-24.939,181.013-74.837C536.715,337.099,536.715,174.688,436.896,74.869z     M361.461,331.317c8.341,8.341,8.341,21.824,0,30.165c-4.16,4.16-9.621,6.251-15.083,6.251c-5.461,0-10.923-2.091-15.083-6.251    l-75.413-75.435l-75.392,75.413c-4.181,4.16-9.643,6.251-15.083,6.251c-5.461,0-10.923-2.091-15.083-6.251    c-8.341-8.341-8.341-21.845,0-30.165l75.392-75.413l-75.413-75.413c-8.341-8.341-8.341-21.845,0-30.165    c8.32-8.341,21.824-8.341,30.165,0l75.413,75.413l75.413-75.413c8.341-8.341,21.824-8.341,30.165,0    c8.341,8.32,8.341,21.824,0,30.165l-75.413,75.413L361.461,331.317z"
+                  fill="#718096"
+                  data-original="#000000"
+                ></path>
+              </g>
+            </svg>
+          </button>
+        </section>
+      </div>
+    </div>
   </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+const API_URL = "https://min-api.cryptocompare.com/data";
+const API_KEY =
+  "d594f9f2d553c6aa53ba9ac39aac53f6e7f8d9ade85ef0635560f57203915fe9";
 
-nav {
-  padding: 30px;
+export default {
+  name: "App",
+  data() {
+    return {
+      ticker: "",
+      tickers: [],
+      sel: null,
+      selStats: null,
+      matches: [],
+      cryptoNames: [],
+      isExisting: false
+    };
+  },
+  created() {
+    const tickersData = localStorage.getItem("tickersList");
+    if (tickersData) {
+      this.tickers = JSON.parse(tickersData);
+      this.tickers.forEach((t) => this.subscribeToUpdates(t.name));
+    }
+  },
+  mounted() {
+    fetch(`${API_URL}/all/coinlist?summary=true`)
+      .then((res) => res.json())
+      .then((data) => {
+        const names = [];
+        for (const key of Object.entries(data.Data)) {
+          names.push(key[0]);
+        }
+        this.cryptoNames = names;
+      });
+  },
+  methods: {
+    handleAdd() {
+      this.isAlredyExisting();
+      if (this.isExisting) return;
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
+      const currentTicker = {
+        name: this.ticker.toUpperCase(),
+        price: "-"
+      };
 
-    &.router-link-exact-active {
-      color: #42b983;
+      this.tickers.push(currentTicker);
+      localStorage.setItem("tickersList", JSON.stringify(this.tickers));
+      this.subscribeToUpdates(currentTicker.name);
+      this.resetData();
+    },
+    subscribeToUpdates(tickerName) {
+      setInterval(async () => {
+        const f = await fetch(
+          `${API_URL}/price?fsym=${tickerName}&tsyms=USD&${API_KEY}`
+        );
+        const data = await f.json();
+        const price = data.USD;
+        this.tickers.find((t) => t.name === tickerName).price =
+          price > 1 ? price.toFixed(2) : price.toPrecision(2);
+      }, 5000);
+    },
+    handleRemove(tickerToRemove) {
+      this.tickers = this.tickers.filter((t) => t !== tickerToRemove);
+      this.sel = null;
+    },
+    async handleSelect(tickerSelected) {
+      this.sel = tickerSelected;
+
+      const f = await fetch(
+        `${API_URL}/generateAvg?fsym=${this.sel.name}&tsym=USD&e=Kraken&${API_KEY}`
+      );
+      const data = await f.json();
+      const { VOLUME24HOURTO, OPEN24HOUR, LOW24HOUR, HIGH24HOUR } = data.RAW;
+
+      const stats = [
+        { title: "Market Cap", price: VOLUME24HOURTO },
+        { title: "Open 24h", price: OPEN24HOUR },
+        { title: "Low 24h", price: LOW24HOUR },
+        { title: "High 24h", price: HIGH24HOUR }
+      ];
+
+      this.selStats = stats.map((stat) => {
+        const price = stat.price;
+        return {
+          ...stat,
+          price:
+            price > 1
+              ? price > 1000
+                ? Math.abs(price) > 10000
+                  ? Math.sign(price) * (Math.abs(price) / 1000).toFixed(1) + "K"
+                  : Math.sign(price) * Math.abs(price)
+                : price.toFixed(2)
+              : price.toPrecision(2)
+        };
+      });
+    },
+    handleMatch() {
+      if (this.ticker === "") {
+        this.resetData();
+        return;
+      }
+      const reg = new RegExp("^" + this.ticker.trim(), "i");
+      this.matches = this.cryptoNames
+        .filter((name) => reg.test(name))
+        .sort()
+        .slice(0, 4);
+    },
+    isAlredyExisting() {
+      this.isExisting =
+        this.tickers.filter(
+          (ticker) => ticker.name === this.ticker.toUpperCase()
+        ).length === 0
+          ? false
+          : true;
+    },
+    resetData() {
+      this.matches = [];
+      this.isExisting = false;
+      this.matches = [];
     }
   }
-}
-</style>
+};
+</script>
